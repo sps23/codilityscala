457bf58cdd278e8b8c5ae023ffde1a9eb907b8cf
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2016-08-22 18:40:06 +0100

    Lesson2: OddOccurrencesInArray - partial solution 88%, refactoring.

--- src/main/scala/silverstar/codility/lessons/lesson2/OddOccurrencesInArray.scala
+++ src/main/scala/silverstar/codility/lessons/lesson2/OddOccurrencesInArray.scala
@@ -1,8 +1,10 @@
 package silverstar.codility.lessons.lesson2
 
+/**
+  * Solution for task: OddOccurrencesInArray.
+  */
 object OddOccurrencesInArray {
 
-  def oddOccurrence(a: Array[Int]): Int = {
-    a.toList.groupBy(e => e).filter(p => p._2.length % 2 == 1).keys.head
-  }
+  def oddOccurrence(a: Array[Int]): Int = a.groupBy(e => e).filter(p => p._2.length % 2 == 1).keys.head
+
 }
--- src/test/scala/silverstar/codility/lessons/lesson2/OddOccurrencesInArraySpec.scala
+++ src/test/scala/silverstar/codility/lessons/lesson2/OddOccurrencesInArraySpec.scala
@@ -3,6 +3,9 @@ package silverstar.codility.lessons.lesson2
 import org.scalatest.FunSpec
 import org.scalatest.Matchers._
 
+/**
+  * Tests for OddOccurrencesInArray.
+  */
 class OddOccurrencesInArraySpec extends FunSpec {
 
   describe("OddOccurrencesInArray oddOccurrence") {
@@ -13,6 +16,17 @@ class OddOccurrencesInArraySpec extends FunSpec {
 
     it("returns 9 for [9, 3, 9, 3, 9, 7, 9, 7, 9]") {
       OddOccurrencesInArray.oddOccurrence(Array(9, 3, 9, 3, 9, 7, 9, 7, 9)) shouldBe 9
+    }
+
+    it("returns correct result for big array with multiple repetitions") {
+      val testArray = (1 to 10000).toArray
+      val array: Array[Int] = testArray ++ testArray ++ testArray ++ testArray ++ testArray ++ testArray :+ 1
+      OddOccurrencesInArray.oddOccurrence(array) shouldBe 1
+    }
+
+    it("returns correct result for big array") {
+      val array: Array[Int] = (1 to 50000).toArray ++ (1 to 50000) :+ 1
+      OddOccurrencesInArray.oddOccurrence(array) shouldBe 1
     }
   }
 }
