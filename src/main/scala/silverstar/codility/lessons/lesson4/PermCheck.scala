0a705499d191a114c4c2802aa9501e7ba2da3820
Sylwester Stocki <sylwester.stocki.rnd@gmail.com>
2016-08-23 18:32:14 +0100

    Lesson4: PermCheck - solution.

--- src/main/scala/silverstar/codility/lessons/lesson4/PermCheck.scala
+++ src/main/scala/silverstar/codility/lessons/lesson4/PermCheck.scala
@@ -2,6 +2,14 @@ package silverstar.codility.lessons.lesson4
 
 object PermCheck {
 
-  def isPermutation(a: Array[Int]): Int = if ((1 to a.length).sum == a.sum) 1 else 0
+  def isPermutation(a: Array[Int]): Int = {
+    val crossOut: Array[Boolean] = (1 to a.length).map(e => false).toArray
+
+    a.foreach{ elem =>
+      if(elem <= crossOut.length) crossOut.update(elem - 1, true)
+    }
+
+    if (crossOut.contains(false)) 0 else 1
+  }
 
 }
--- src/test/scala/silverstar/codility/lessons/lesson4/PermCheckSpec.scala
+++ src/test/scala/silverstar/codility/lessons/lesson4/PermCheckSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.Matchers._
 
 class PermCheckSpec extends FunSpec {
 
-  describe("PermCheck ") {
+  describe("PermCheck isPermutation") {
 
     it("returns 1 for [4, 1, 3, 2]") {
       PermCheck.isPermutation(Array(4, 1, 3, 2)) shouldBe 1
@@ -25,8 +25,18 @@ class PermCheckSpec extends FunSpec {
       PermCheck.isPermutation(array) shouldBe 0
     }
 
-    it("returns 0 for big array of 100,000 elements filled with element equal 100,000") {
-      val array: Array[Int] = (1 to 100000).map(e => 1000000000).toArray
+    it("returns 0 for big array of 100,000 elements all the same values equal to 100,000") {
+      val array: Array[Int] = (1 to 100000).map(e => 100000).toArray
+      PermCheck.isPermutation(array) shouldBe 0
+    }
+
+    it("returns 0 when total sum is correct and it is not permutation for [4, 1, 4, 1]") {
+      PermCheck.isPermutation(Array(4, 1, 4, 1)) shouldBe 0
+    }
+
+    it("returns 0 when total sum is correct and it is not permutation for big array") {
+      val array1: Array[Int] = (2 to 100000).map(e => 1).toArray
+      val array: Array[Int] = array1 :+ ((1 to 100000).sum - array1.sum)
       PermCheck.isPermutation(array) shouldBe 0
     }
   }
