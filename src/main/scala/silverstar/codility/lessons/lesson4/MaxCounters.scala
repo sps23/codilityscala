bb5e94504c9069dabe44c1efde94c1e2379a8373
sstocki <sylwester.stocki.rnd@gmail.com>
2016-11-06 21:29:56 +0000

    Lesson4: MaxCounters - solution.

---
+++ src/main/scala/silverstar/codility/lessons/lesson4/MaxCounters.scala
@@ -0,0 +1,43 @@
+package silverstar.codility.lessons.lesson4
+
+import scala.annotation.tailrec
+
+object MaxCounters {
+
+  def calculateCountersSlow(n: Int, a: Array[Int]): Array[Int] = {
+
+    @tailrec
+    def iterate(max: Int, i: Int, counters: Array[Int]): Array[Int] = {
+      if (i < a.length) {
+        val newMax: Int = if (a(i) >= 1 && a(i) <= n) {
+          counters(a(i) - 1) = counters(a(i) - 1) + 1
+          Math.max(counters(a(i) - 1), max)
+        } else {
+          counters.transform(_ => max)
+          max
+        }
+        iterate(newMax, i + 1, counters)
+      } else counters
+    }
+
+    iterate(0, 0, Array.ofDim[Int](n))
+  }
+
+  def calculateCounters(n: Int, a: Array[Int]): Array[Int] = {
+
+    @tailrec
+    def iterate(max: Int, toAdd: Int, i: Int, counters: Array[Int]): Array[Int] = {
+      if (i < a.length) {
+        val index = a(i) - 1
+        val (newMax, newToAdd) =
+          if (a(i) >= 1 && a(i) <= n) {
+            counters(index) = Math.max(counters(index), toAdd) + 1
+            (Math.max(counters(index), max), toAdd)
+          } else (max, max)
+        iterate(newMax, newToAdd, i + 1, counters)
+      } else counters.map(e => Math.max(e, toAdd))
+    }
+
+    iterate(0, 0, 0, Array.ofDim[Int](n))
+  }
+}
