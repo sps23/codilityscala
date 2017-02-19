1543241f77f21cdddec8c7feb3e9374ead44b3c6
sstocki <sylwester.stocki.rnd@gmail.com>
2017-02-19 18:05:23 +0000

    Lesson10: MinPerimeterRectangle - solution.

---
+++ src/main/scala/silverstar/codility/lessons/lesson10/MinPerimeterRectangle.scala
@@ -0,0 +1,48 @@
+package silverstar.codility.lessons.lesson10
+
+import scala.annotation.tailrec
+
+object MinPerimeterRectangle {
+
+  def perimeter(a: Int, b: Int): Int = 2 * (a + b)
+
+  def minPerimeter(n: Int): Int = {
+
+    @tailrec
+    def iterate(i: Int, min: Int): Int = {
+      if (i.toLong * i.toLong < n.toLong)
+        if (n % i == 0) iterate(i + 1, Math.min(min, perimeter(i, n / i)))
+        else iterate(i + 1, min)
+      else if (i.toLong * i.toLong == n) Math.min(min, perimeter(i, i))
+      else min
+    }
+
+    iterate(1, Int.MaxValue)
+  }
+
+  def minPerimeterSlow(n: Int): Int = {
+
+    @tailrec
+    def allSides(i: Int, f: Seq[(Int, Int)]): Seq[(Int, Int)] = {
+      if (i.toLong * i.toLong < n.toLong)
+        if (n % i == 0) allSides(i + 1, (i, n / i) +: f)
+        else allSides(i + 1, f)
+      else if (i.toLong * i.toLong == n) (i, i) +: f
+      else f
+    }
+
+    val s: Seq[(Int, Int)] = allSides(1, Seq())
+    perimeter(s.head._1, s.head._2)
+  }
+
+  def minPerimeterFold(n: Int): Int = {
+
+    (1 to Math.sqrt(n).toInt).fold(Int.MaxValue)((min, i) => {
+      if (i.toLong * i.toLong < n.toLong)
+        if (n % i == 0) Math.min(min, perimeter(i, n / i))
+        else min
+      else if (i.toLong * i.toLong == n) Math.min(min, perimeter(i, i))
+      else min
+    })
+  }
+}
---
+++ src/test/scala/silverstar/codility/lessons/lesson10/MinPerimeterRectangleSpec.scala
@@ -0,0 +1,43 @@
+package silverstar.codility.lessons.lesson10
+
+import org.scalatest.{FunSpec, Matchers}
+
+class MinPerimeterRectangleSpec extends FunSpec with Matchers {
+
+  import MinPerimeterRectangle._
+
+  private val MinN = 1
+  private val MaxN = 1000000000
+
+  val testData: Seq[(Int, Int)] = Seq(
+    (30, 22),
+    (31, 2 * (1 + 31)),
+    (100, 2 * (10 + 10)),
+    (MinN, 2 * (MinN + MinN)),
+    (MaxN, 2 * (31250 + 32000))
+  )
+
+  describe("minPerimeter") {
+    for (t <- testData) {
+      it(s"should return '${t._2}' for ${t._1}") {
+        minPerimeter(t._1) shouldBe t._2
+      }
+    }
+  }
+
+  describe("minPerimeterSlow") {
+    for (t <- testData) {
+      it(s"should return '${t._2}' for ${t._1}") {
+        minPerimeterSlow(t._1) shouldBe t._2
+      }
+    }
+  }
+
+  describe("minPerimeterFold") {
+    for (t <- testData) {
+      it(s"should return '${t._2}' for ${t._1}") {
+        minPerimeterFold(t._1) shouldBe t._2
+      }
+    }
+  }
+}
