586cefe12998a1d0d83aa1ef628bad030f01ef8f
sstocki <sylwester.stocki.rnd@gmail.com>
2017-02-19 17:55:20 +0000

    Lesson6: MaxProductOfThree - solution.

---
+++ src/main/scala/silverstar/codility/lessons/lesson6/MaxProductOfThree.scala
@@ -0,0 +1,41 @@
+package silverstar.codility.lessons.lesson6
+
+object MaxProductOfThree {
+
+  def maxProductTriplet(a: Array[Int]): Int = {
+
+    val (aNegativeAsc, aPositiveAsc) = a.sorted.partition(_ < 0)
+    val aPositiveDesc = aPositiveAsc.reverse
+    val aNegativeDesc = aNegativeAsc.reverse
+
+    val productWithAllPositive: Option[Int] =
+      if (aPositiveDesc.length > 2)
+        Option(aPositiveDesc.head * aPositiveDesc.tail.head * aPositiveDesc.tail.tail.head)
+      else None
+
+    val productWith2Negative: Option[Int] =
+      if (aNegativeAsc.length > 1 && !aPositiveDesc.isEmpty)
+        Option(aNegativeAsc.head * aNegativeAsc.tail.head * aPositiveDesc.head)
+      else None
+
+    (productWithAllPositive, productWith2Negative) match {
+      case (Some(i), Some(j)) => Math.max(i, j)
+      case (Some(i), None) => i
+      case (None, Some(j)) => j
+      case _ =>
+        val productWithAllNegative =
+          if (aNegativeDesc.length > 2)
+            Option(aNegativeDesc.head * aNegativeDesc.tail.head * aNegativeDesc.tail.tail.head)
+          else None
+        val productWith1Negative =
+          if (aPositiveAsc.length > 1 && !aNegativeDesc.isEmpty)
+            Option(aPositiveAsc.head * aPositiveAsc.tail.head * aNegativeDesc.head)
+          else None
+        (productWithAllNegative, productWith1Negative) match {
+          case (Some(i), Some(j)) => Math.max(i, j)
+          case (Some(i), None) => i
+          case (None, Some(j)) => j
+        }
+    }
+  }
+}
---
+++ src/test/scala/silverstar/codility/lessons/lesson6/MaxProductOfThreeSpec.scala
@@ -0,0 +1,34 @@
+package silverstar.codility.lessons.lesson6
+
+import org.scalatest.{FunSpec, Matchers}
+
+class MaxProductOfThreeSpec extends FunSpec with Matchers {
+
+  import MaxProductOfThree._
+
+  private val MinN = 3
+  private val MaxN = 100000
+  private val MinA = -10000
+  private val MaxA = 10000
+
+  val testData: Seq[(Array[Int], Int)] = Seq(
+    (Array(-3, 1, 2, -2, 5, 6), 60),
+    (Array(-2, 1, 5, -3, 2, 6), 60),
+    (Array(-3, -1, -2, -2, -5, -6), -4),
+    (Array(3, 1, 2, 2, 5, 6), 90),
+    (Array(-3, -2, 1, 3, 4), 24),
+    ((1 to MinN).toArray, 6),
+    ((1 to MinN).map(_ * -1).toArray, -6),
+    ((1 to MaxN).toArray, MaxN * (MaxN - 1) * (MaxN - 2)),
+    ((1 to MaxN).reverse.toArray, MaxN * (MaxN - 1) * (MaxN - 2)),
+    ((MinA to MaxA).toArray, -MaxA * (-MaxA + 1) * MaxA)
+  )
+
+  describe("maxProductTriplet") {
+    for (t <- testData) {
+      it(s"should return '${t._2}' for ${t._1.mkString("[", ";", "]")}") {
+        maxProductTriplet(t._1) shouldBe t._2
+      }
+    }
+  }
+}
